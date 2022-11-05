8888cdd9997b911730e718c839a2fddada7bc512
Sergey Chelak <sergey.chelak@gmail.com>
2022-11-05 15:28:55 +0200

    formatter with options

--- src/machine/debug.rs
+++ src/machine/debug.rs
@@ -4,7 +4,7 @@ use std::fmt::Debug;
 pub enum DebugToken {
     Address(usize),
     Operation(u16),
-    Value(u16, Option<usize>),  // value, optional register index
+    Value(u16, Option<usize>), // value, optional register index
     RegisterIdx(usize),
     Comment(String),
 }
@@ -24,88 +24,100 @@ pub trait DebugOutput {
     fn complete(&self);
 }
 
-pub fn format_debug_tokens(tokens: &Vec<DebugToken>) -> String {    
-    let fmt_instr = format_instructions(tokens);
-    let fmt_comments = format_comments(tokens);
-    format!("{:50} {}", fmt_instr, fmt_comments)
+pub struct DebugFormatter {
+    instr_width: usize,
+    value_width: usize,
 }
 
-fn format_instructions(tokens: &Vec<DebugToken>) -> String {
-    let instructions: Vec<String> = tokens.iter()
-        .filter(|x| !x.is_comment())
-        .map(|token|
-            match *token {
-                DebugToken::Address(address) => format_address(address),
-                DebugToken::Operation(op_code) => format_operation(op_code),
-                DebugToken::Value(value, reg_idx) => format_value(value, reg_idx),
-                DebugToken::RegisterIdx(idx) => format_register_idx(idx),
-                _ => "".to_string(),
-            }
-        )
-        .collect();
-
-    instructions.join(" ")
-}
-
-fn format_address(address: usize) -> String {
-    format!("{:<9}", address)
-}
-
-fn format_operation(op_code: u16) -> String {
-    let name = match op_code {
-        0 => "halt",
-        1 => "set",
-        2 => "push",
-        3 => "pop",
-        4 => "eq",
-        5 => "gt",
-        6 => "jmp",
-        7 => "jt",
-        8 => "jf",
-        9 => "add",
-        10 => "mult",
-        11 => "mod",
-        12 => "and",
-        13 => "or",
-        14 => "not",
-        15 => "rmem",
-        16 => "wmem",
-        17 => "call",
-        18 => "ret",
-        19 => "out",
-        20 => "in",
-        21 => "noop",
-        _ => "???"
-    };
-    format!("{:>7}", name)
-}
-
-fn format_value(value: u16, reg_idx_opt: Option<usize>) -> String {
-    let mut comps: Vec<String> = Vec::new();
-    if let Some(reg_idx) = reg_idx_opt {
-        comps.push(format_register_idx(reg_idx));
+impl DebugFormatter {
+    pub fn new() -> Self {
+        Self {
+            instr_width: 50,
+            value_width: 9,
+        }
     }
-    comps.push(format!("{value}"));
-    format!("{:<9}", comps.join(""))
-}
 
-fn format_register_idx(idx: usize) -> String {
-    format!("[{idx}]")
-}
+    pub fn format_debug_tokens(&self, tokens: &Vec<DebugToken>) -> String {
+        let fmt_instr = self.format_instructions(tokens);
+        let fmt_comments = self.format_comments(tokens);
+        format!("{:width$} {}", fmt_instr, fmt_comments, width = self.instr_width)
+    }
 
-fn format_comments(tokens: &Vec<DebugToken>) -> String {
-    let array: Vec<String> = tokens.iter()
-        .filter(|x| x.is_comment())
-        .map(|x| {
-            match x {
+    fn format_instructions(&self, tokens: &Vec<DebugToken>) -> String {
+        let instructions: Vec<String> = tokens
+            .iter()
+            .filter(|x| !x.is_comment())
+            .map(|token| match *token {
+                DebugToken::Address(address) => self.format_address(address),
+                DebugToken::Operation(op_code) => self.format_operation(op_code),
+                DebugToken::Value(value, reg_idx) => self.format_value(value, reg_idx),
+                DebugToken::RegisterIdx(idx) => self.format_register_idx(idx),
+                _ => "".to_string(),
+            })
+            .collect();
+
+        instructions.join(" ")
+    }
+
+    fn format_address(&self, address: usize) -> String {
+        format!("{:<width$}", address, width = self.value_width)
+    }
+
+    fn format_operation(&self, op_code: u16) -> String {
+        let name = match op_code {
+            0 => "halt",
+            1 => "set",
+            2 => "push",
+            3 => "pop",
+            4 => "eq",
+            5 => "gt",
+            6 => "jmp",
+            7 => "jt",
+            8 => "jf",
+            9 => "add",
+            10 => "mult",
+            11 => "mod",
+            12 => "and",
+            13 => "or",
+            14 => "not",
+            15 => "rmem",
+            16 => "wmem",
+            17 => "call",
+            18 => "ret",
+            19 => "out",
+            20 => "in",
+            21 => "noop",
+            _ => "???",
+        };
+        format!("{:>7}", name)
+    }
+
+    fn format_value(&self, value: u16, reg_idx_opt: Option<usize>) -> String {
+        let mut comps: Vec<String> = Vec::new();
+        if let Some(reg_idx) = reg_idx_opt {
+            comps.push(self.format_register_idx(reg_idx));
+        }
+        comps.push(format!("{value}"));
+        format!("{:<width$}", comps.join(""), width = self.value_width)
+    }
+
+    fn format_register_idx(&self, idx: usize) -> String {
+        format!("[{idx}]")
+    }
+
+    fn format_comments(&self, tokens: &Vec<DebugToken>) -> String {
+        let array: Vec<String> = tokens
+            .iter()
+            .filter(|x| x.is_comment())
+            .map(|x| match x {
                 DebugToken::Comment(text) => text.clone(),
                 _ => "".to_string(),
-            }
-        })
-        .collect();
-    if array.len() > 0 {
-        format!("; {}", array.join(", "))
-    } else {
-        "".to_string()
+            })
+            .collect();
+        if array.len() > 0 {
+            format!("; {}", array.join(", "))
+        } else {
+            "".to_string()
+        }
     }
 }
--- src/machine/debug_writer.rs
+++ src/machine/debug_writer.rs
@@ -2,20 +2,20 @@ use super::debug::*;
 
 // fake writer
 pub struct StubDebugWriter {
-
+    formatter: DebugFormatter,
 }
 
 impl StubDebugWriter {
     pub fn new() -> Self {
         Self {
-            //
+            formatter: DebugFormatter::new(),
         }
     }
 }
 
 impl DebugOutput for StubDebugWriter {
     fn write(&self, tokens: &Vec<DebugToken>) {
-        //
+        println!("{}", self.formatter.format_debug_tokens(tokens));
     }
 
     fn complete(&self) {
