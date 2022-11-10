e9de6de2d8faf8ea0e6895de42060ae0a2bee072
Sergey Chelak <sergey.chelak@gmail.com>
2022-11-10 23:36:32 +0200

    added comment

--- playground/confirmation/main.rs
+++ playground/confirmation/main.rs
@@ -1,4 +1,27 @@
-// mod alg;
+/*
+Run with command:
+    RUST_MIN_STACK=33554432 cargo run
+
+5483          set [r0] 4                           ; r0 = 4
+5486          set [r1] 1                           ; r1 = 1
+5489         call @6027
+6027           jt [r0] @6035                       ; if [r0] != 0 jump 6035
+6030          add [r0] [r1]  1                     ; r0 = r1 + 1
+6034          ret
+6035           jt [r1] @6048                       ; if [r1] != 0 jump 6048
+6038          add [r0] [r0]  32767                 ; r0 = (r0 + 32767) % 32768
+6042          set [r1] [r7]                        ; reg[1] = reg[7]
+6045         call @6027                            ; jump to 6027
+6047          ret
+6048         push [r0]
+6050          add [r1] [r1]  32767                 ; r1 = (r1 + 32767) % 32768
+6054         call @6027                            ; jump to 6027
+6056          set [r1] [r0]                        ; r1 = r0
+6059          pop [r0]
+6061          add [r0] [r0]  32767                 ; r0 = r0 + 32767) % 32768
+6065         call @6027                            ; jump to 6027
+6067          ret
+*/
 use std::thread::{self, JoinHandle};
 use std::collections::HashMap;
 
